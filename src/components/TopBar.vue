fa1167e5c25d4d1fe70ea162918a3633a87396de
= <=>
2025-02-22 16:01:33 +0530

    added background

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -9,9 +9,13 @@
 header {
   background: #222;
   color: white;
-  padding: 15px;
+  padding:0;
+  top:0;
+  left:0;
+  margin:0;
   display: flex;
-  align-items: center;
+  width:100%;
+  align-items:center;
   justify-content: center;
   position: relative;
   font-size: 1.5rem;
